59842bc684fb8711238215bc4833bc583237b8df
Daniel-Hacking <daniel.hacking123@gmail.com>
2016-10-18 21:13:09 -0600

    First Start
    Needs more work

---
+++ tic.py
@@ -0,0 +1,23 @@
+def board():
+    print (' '+x[1] and +y[1]' | '+x[2] and +y[1] )' | '+x[3] and +y[1])
+    print ('---|---|---')
+    print (' '+x[1] and +y[2]' | '+x[2] and +y[2] )' | '+x[3] and +y[2])
+    print ('---|---|---')
+    print (' '+x[1] and +y[3]' | '+x[2] and +y[3] )' | '+x[3] and +y[3])
+
+def end():
+    
+    if 
+
+def input(board):
+    x = none
+    y = none
+    while end == False:
+        print board(x, y)
+        x = input("X = ")
+        y = input("Y = ")
+        if space_taken != False:
+            print ('Try again my man')
+        else:
+            spaces_left -= 1
+        
