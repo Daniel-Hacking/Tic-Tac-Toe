25f18cbf66ee112723e6fef969c4917debea5773
Daniel-Hacking <daniel.hacking123@gmail.com>
2016-10-21 00:11:28 -0600

    goes all the way through
    -added notes
    -improved move (player input) function
    -added x or o funtion

--- tic.py
+++ tic.py
@@ -1,14 +1,16 @@
-def 
-    a = ' '
-    b = ' '
-    c = ' '
-    d = ' '
-    e = ' '
-    f = ' '
-    g = ' '
-    h = ' '
-    i = ' '
+#values for spaces
+a = ' '
+b = ' '
+c = ' '
+d = ' '
+e = ' '
+f = ' '
+g = ' '
+h = ' '
+i = ' '
+turn = 1
 
+#prints board
 def board(x, y):
     print (' {0} | {1} | {2} '.format(a, b, c))
     print ('---|---|---')
@@ -16,52 +18,48 @@ def board(x, y):
     print ('---|---|---')
     print (' {0} | {1} | {2} '.format(g, h, i))
 
-def x_or_o():
-    turn = 1
+#what determines if an X or an O is used
+def x_or_o(turn):
     if turn % 2 == 0:
         return "O"
     else:
         return "X"
 
-def space_taken():
-    if 
-
-
-def move(board):
+#handles player input
+def move(board, turn):
     x = 0
     y = 0
     spaces_left = 9
     while spaces_left > 0:
+        print (turn)
         print (board(x, y))
         x = input("X = ")
         y = input("Y = ")
-        if space_taken != False:
-            print ('Try again my man')
-        else:
-            if x<0 and x>4 and y<0 and y>4:
-                if x == 1:
-                    if y == 1:
-                        a = x_or_o
-                    elif y == 2:
-                        b = x_or_o
-                    else:
-                        c = x_or_o
-                elif x == 2:
-                    if y == 1:
-                        d = x_or_o
-                    elif y == 2:
-                        e = x_or_o
-                    else:
-                        f = x_or_o
+        if int(x)>0 and int(x)<4 and int(y)>0 and int(y)<4:
+            if x == 1:
+                if y == 1:
+                    a = x_or_o(turn)
+                elif y == 2:
+                    b = x_or_o(turn)
                 else:
-                    if y == 1:
-                        g = x_or_o
-                    elif y == 2:
-                        h = x_or_o
-                    else:
-                        i = x_or_o
+                    c = x_or_o(turn)
+            elif x == 2:
+                if y == 1:
+                    d = x_or_o(turn)
+                elif y == 2:
+                    e = x_or_o(turn)
+                else:
+                    f = x_or_o(turn)
             else:
-                False
-            turn -= 1
+                if y == 1:
+                    g = x_or_o(turn)
+                elif y == 2:
+                    h = x_or_o(turn)
+                else:
+                    i = x_or_o(turn)
+            turn += 1
             spaces_left -= 1
+        else:
+            print ('Not a real space my dude')
 
+move(board, turn)
